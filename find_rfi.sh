ddb6e3618dbeea139877ef7efdfc3279e490f873
Andrey Kos <andrey.kosmynin@gmail.com>
2018-04-16 15:03:43 +0300

    Add while

--- find_rfi.sh
+++ find_rfi.sh
@@ -5,8 +5,6 @@
 #@include "\x2fhom\x65/ad\x6din/\x77eb/\x6eako\x6cesa\x68.ne\x74.ua\x2fpub\x6cic_\x68tml\x2fwa-\x63ach\x65/ap\x70s/c\x6fnta\x63ts/\x66avi\x63on_\x615ac\x34a.i\x63o";
 #
 #/*f6996*/
-#long space
-#favicon_c45b08.ico
 
 SITE_PATH=$1
 
@@ -30,12 +28,26 @@ for i in `find ${SITE_PATH} -type f -name "*.php" |xargs egrep -ril "^\/\*[[:aln
 	#clear file if is only <?php ?>  or <?php 
 	#sed -i '/^<?php\s?>$'
 done
-echo "Find and remove long space"
+
+#Find and remove long space pattern <?php                    
+echo Find and remove long space...
 find ${SITE_PATH} -type f -name '*.php' |xargs perl -pi -e's/^<\?php\s{100,}.*$/<\?php/' 2>/dev/null && echo "OK" || exit 1
 
-echo "Find and remove favicon_c45b08.ico"
+echo Find and remove pattern favicon_c45b08.ico
 find {SITE_PATH} -type f -name "favicon_*.ico" -exec rm -rfv {} \; && echo "OK" || exit 1
 
-echo "Change permision"
-find ${SITE_PATH} -type f -exec chmod 644 {} \; && echo "Change permision files OK" || exit 1
-find ${SITE_PATH} -type d -exec chmod 755 {} \; && echo "Change permision dyrectory OK" || exit 1
+while true; do
+	read -p "Do you want to change permissions? (yes/no): "  I
+	if [[ $I = "yes" ]]; then
+		echo -n Change permissions...
+	    find ${SITE_PATH} -type f -exec chmod 644 {} \; && echo "Files OK" || exit 1
+	    find ${SITE_PATH} -type d -exec chmod 755 {} \; && echo "Directories OK" || exit 1
+	    break
+	elif [[ $I = "no" ]]; then
+	    echo Stop change permissions
+	    break
+	else
+		echo Please enter either \"yes\" or \"no\"
+		continue
+	fi
+done
